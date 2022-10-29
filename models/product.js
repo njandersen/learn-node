b866b8815f982468fc106a206632b1995a182a17
njandersen <njandersen22@gmail.com>
2022-10-29 06:43:50 -0400

    add model part of MVC

---
+++ models/product.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const path = require("path");
+
+const pathName = require("../util/path");
+
+const p = path.join(pathName, "data", "products.json");
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      let products;
+      try {
+        products = JSON.parse(fileContent);
+      } catch {
+        products = [];
+      }
+      cb(products);
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
